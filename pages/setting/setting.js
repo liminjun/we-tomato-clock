d2fe3c7a885818e0f5fc44542841e78b3e747447
liminjun <v-leeli@hotmail.com>
2017-09-04 18:33:48 +0800

    feat:设置可以保存到后台

--- pages/setting/setting.js
+++ pages/setting/setting.js
@@ -1,33 +1,97 @@
 var app = getApp();
 Page({
   data: {
-    profile: null
+    profile: null,
+    workTime:25,
+    restTime:5,
+    recordId:null
   },
   onLoad(options) {
     var result = app.getUserInfo();
     this.setData({
       profile: app.getUserInfo()
-    })
+    });
+
+    this.getSettingData();
   },
-  onShow: function() {
-    wx.setNavigationBarTitle({
-      title: '设置'
-    })
+  onShow: function () {
+    
+  },
+  onHide:function(){
+    console.log("page hide.");
+    //用户设置了时长，再页面隐藏的时候保存到后台
+
+  },
+  getSettingData: function () {
+    var that=this;
+    let objects = {
+      tableID: 1323,
+      userId: app.getUserId()
+    };
+    wx.BaaS.getRecordList(objects).then((res) => {
+      
+      that.setData({
+        workTime: res.data.objects[0].taskMinutes,
+        restTime: res.data.objects[0].restMinutes,
+        recordId:res.data.objects[0].id
+      })
+    }, (err) => {
+      // err
+      console.dir(err);
+    });
+  },
+  changeWorkTime: function (e) {
     this.setData({
-    	workTime: wx.getStorageSync('workTime'),
-    	restTime: wx.getStorageSync('restTime')
+      workTime:  e.detail.value
+    })
+
+    wx.setStorage({
+      key: 'workTime',
+      data: e.detail.value
     })
   },
-  changeWorkTime: function(e) {
-  	wx.setStorage({
-  		key: 'workTime',
-  		data: e.detail.value
-  	})
+  changeRestTime: function (e) {
+    this.setData({
+      restTime:  e.detail.value
+    })
+    wx.setStorage({
+      key: 'restTime',
+      data: e.detail.value
+    })
   },
-  changeRestTime: function(e) {
-  	wx.setStorage({
-  		key: 'restTime',
-  		data: e.detail.value
-  	})
-  }
+  //添加反馈
+  saveSetting(e) {
+
+    let that = this
+    let tableID = 1323; //setting表ID
+    debugger;
+    let recordID=that.data.recordId;
+
+    let taskMinutes=that.data.workTime;
+    let restMinutes=that.data.restTime;
+
+    let data = {
+      taskMinutes: parseFloat(taskMinutes),
+      restMinutes: parseFloat(restMinutes)
+    }
+    let objects = {
+      tableID,
+      recordID,
+      data
+    }
+
+    // 编辑配置
+    wx.BaaS.updateRecord(objects).then((res) => {
+
+      wx.showToast({
+        title: '保存成功',
+        icon: 'success',
+        duration: 2000
+      });
+
+
+    }, (err) => {
+      console.log(err)
+    })
+  },
 })
